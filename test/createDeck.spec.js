4ff00a2f2f0e773a31f217630d792bd40fb8b327
lzys90 <leongyzs@gmail.com>
2016-12-24 01:19:07 +0800

    Fix createDeck test

--- test/createDeck.spec.js
+++ test/createDeck.spec.js
@@ -1,36 +1,19 @@
-/*
 'use strict';
 
+var sinon = require('sinon');
 const expect = require('chai').expect;
 const createDeck = require('../dist/lib/createDeck');
+const builder = require('botbuilder');
 
-let session = {
-    userData: {
-        location: {
-            longitude: 'testLng',
-            latitude: 'testLat'
-        }
-    }
-};
-let tmpDeck = [];
-let docs = [
-    {properties: {
-        name: [{text: 'testName'}],
-        category: [{text: 'testCategory'}],
-        rating: [{text: 'testRating'}],
-        address: [{text: 'testAddress'}],
-        image: [{src: 'testImage'}]
-    }}
-];
+var session = {};
+var tmpDeck = [];
+var docs = [];
 
 describe('Create Deck', () => {
-    it('should return an array of HeroCard objects', () => {
-        createDeck(session, tmpDeck, docs, 1);
-        expect(tmpDeck).to.be.a('array');
-    });
-    it('should contain parameters extracted from the docs', () => {
-        expect(tmpDeck[0].data.content.title).to.equal('testName');
-        expect(tmpDeck[0].data.content.subtitle).to.equal('testCategory, testRating, testAddress');
+    it('should return an array', () => {
+        var stub = sinon.stub(builder, 'HeroCard').returns({});
+        var tmpDeck2 = createDeck(session, tmpDeck, docs, 1);  // side effect is to create an array from a list of objects
+        stub.restore();
+        expect(tmpDeck2).to.be.a('array');
     });
 });
-*/
